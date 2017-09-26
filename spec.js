85400d0fce6daa036e8d9b6a26a2f2758b6fb8cf
Connor Peshek <connorpeshek@gmail.com>
2017-09-25 22:57:48 -0400

    Replace Mocha with Tape.

--- spec.js
+++ spec.js
@@ -1,15 +1,13 @@
+var test = require('tape')
 var request = require('supertest')
-describe('loading express', function () {
-  var server
-  beforeEach(function () {
-    server = require('./app.js')
-  })
-  afterEach(function () {
-    server.close()
-  })
-  it('404s unset url', function testPath(done) {
-    request(server)
-      .get('/foo/bar')
-      .expect(404, done)
-  })
+var app = require('./app.js')
+
+test('404', function (assert) {
+  request(app)
+    .get('/foo')
+    .expect(404)
+    .end(function (err, res) {
+      assert.error(err, 'No error')
+      assert.end()
+    })
 })
