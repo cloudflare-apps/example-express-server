2b3da4f59e9b492922e03fe32bce2238af30f94e
Connor Peshek <connorpeshek@gmail.com>
2017-09-25 22:07:00 -0400

    Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var app = express()
-var axios = require("axios")
+var jsonFetch = require("simple-json-fetch")
 var PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
@@ -10,6 +10,7 @@ app.post('/', function(request, response) {
   // Your install options will be accessable at request.body.install
   // For more information on what the install object will look like, check
   // the request secion of https://www.cloudflare.com/apps/developer/docs/webhooks
+  
 })
 
 app.listen(PORT, function() {
