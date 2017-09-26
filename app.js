bf99f59a93686fd1394ab264f713ead5a628a3c5
Connor Peshek <connorpeshek@gmail.com>
2017-09-25 21:45:38 -0400

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var app = express()
+var axios = require("axios")
+var PORT = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+
+app.post('/', function(request, response) {
+  // Your install options will be accessable at request.body.install
+  // For more information on what the install object will look like, check
+  // the request secion of https://www.cloudflare.com/apps/developer/docs/webhooks
+})
+
+app.listen(PORT, function() {
+  console.log("running on port " + PORT)
+})
